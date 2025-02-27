cddc8d98a7e65cc4f6eb38600ae98a258bfca620
Sergio <a20202117@pucp.edu.pe>
2025-02-27 18:11:24 -0500

    first commit2

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true, // 🔥 Desactiva ESLint en build (No recomendado para producción)
+  },
 };
 
 export default nextConfig;
